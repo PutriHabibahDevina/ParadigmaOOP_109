9ea659c6f79a7e2dbe687df0b0e46e69a8468222
PutriHabibahDevina <putrihdm13@gmail.com>
2023-05-22 15:08:14 +0700

    menampilkan siswa1

--- ParadigmaOOP_109/ParadigmaOOP_109.cpp
+++ ParadigmaOOP_109/ParadigmaOOP_109.cpp
@@ -36,3 +36,10 @@ public:
 	}
 };
 
+int main() {
+	pelajar siswa1("andi laksosno", "SMAN 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
